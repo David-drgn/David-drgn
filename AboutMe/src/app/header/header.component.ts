8a4fb38328e021aa20ad33498fe21317765e4f42
David <david.raphael@SHIFTMC.onmicrosoft.com>
2024-08-18 23:05:28 -0300

    App Bugs

--- AboutMe/src/app/header/header.component.ts
+++ AboutMe/src/app/header/header.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  viewMenu: boolean = false
+  viewMenu: boolean = false;
+  pathSelect: string = '';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) this.pathSelect = event.url;
+    });
+  }
 }
